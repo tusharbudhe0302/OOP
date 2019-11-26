4efd9d04f6cbe6ca02ae5aaa70b981c740965743
Tushar Budhe <tushar.budhe@guycarp.com>
2019-11-26 14:04:28 -0500

    JS Hint

---
+++ CHash/ConsoleTest/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[5] { 1, 2, 3, 4, 5 };
+            int[] b = a;
+            b[0] = 55;
+            Console.WriteLine("Object a");
+            for(int i =0; i < a.Length; i++)
+            {
+                Console.Write(a[i] + " ");
+            }
+
+            Console.WriteLine("Object b");
+            for (int i = 0; i < b.Length; i++)
+            {
+                Console.Write(b[i] + " ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ JS/Objects-JS/arm.js
@@ -0,0 +1,11 @@
+// let greet = ['Hello', 'World'];
+// console.log(greet); // ['Hello', 'World']
+// console.log(...greet); // Hello World
+
+// var a = [0, 1, 2, 3, 4];
+// var b = a; // Copy a to b
+// b[0] = 5;
+// console.log(`Object a`); //[5, 1, 2, 3, 4];
+// console.log(a);
+// console.log(`Object b`); //[5, 1, 2, 3, 4];
+// console.log(b);
